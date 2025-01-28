5360029b800cb23014abd2335bee091a1b155ce2
jerome <jeromeabelida19@gmail.com>
2025-01-28 18:40:26 +0800

    modified: pagination in home controller in index method in barangay

--- app/controllers/barangay/home_controller.rb
+++ app/controllers/barangay/home_controller.rb
@@ -1,6 +1,6 @@
 class Barangay::HomeController < ApplicationController
   before_action :authenticate_barangay_user!
   def index
-    @announcements = Announcement.order(created_at: :desc).page(params[:page]).per(10)
+    @announcements = Announcement.order(created_at: :desc).page(params[:page]).per(2)
   end
 end
