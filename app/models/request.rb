6a2005de8edbce563ca62c9dc4ef7c2e75815775
jerome <jeromeabelida19@gmail.com>
2025-01-28 16:03:25 +0800

    put aasm configuration in request model

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,18 @@
 class Request < ApplicationRecord
+  include AASM
+  belongs_to :user
+
+  aasm column: :state do
+    state :pending, initial: true
+    state :processing
+    state :completed
+
+    event :process do
+      transitions from: :pending, to: :processing
+    end
+
+    event :complete do
+      transitions from: [:pending, :processing], to: :completed
+    end
+  end
 end
